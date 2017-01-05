6cc62bd7929608d8026e74875e3a23622e0018cb
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-05 11:32:10 -0600

    password

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
 var displayCharsRemaining = function(event) {
 
   var current = event.target;
-  var $current = $(current)
+  var $current = $(current);
+
+  var maxChars = event.data.number;
 
   var numChars = current.value.length;
-  var charsLeft = 32 - numChars;
+  var charsLeft = maxChars - numChars;
 
-  if(charsLeft === 32){ 
+  if (charsLeft === maxChars) {
     $current.next().text("");
+  } else {
+    $current.next().text(charsLeft + " characters left!");
   }
-  else{
-      $current.next().text(charsLeft + "characters left!");
-    }
-};
-
-// var charsUsed = ?;
-
-var inputListener = function() {
-  $('input').keyup(displayCharsRemaining);
 
 };
 
+var passwordMatches = function(event) {
 
-$(document).ready(function(){
-  inputListener();
+  var current = event.target;
+  var $current = $(current);
+  var password = $('#password')[0].value;
+  var confirmPassword = current.value;
+
+  if (password !== confirmPassword) {
+    $('#p-matching-message').text("Your password don't match");
+  }
+
+};
+
+var formListener = function() {
+
+  $('textarea').keyup({ number: 140 }, displayCharsRemaining );
+  $('input[type="text"]').keyup({ number: 32 }, displayCharsRemaining );
+  $('input[type="password"]').keyup({ number: 16 }, displayCharsRemaining );
+
+  $('#p-confirmation').keyup( passwordMatches );
+
+};
+
+
+$(document).ready(function() {
+  formListener();
 });
 
