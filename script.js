ed4fd516b2bd2f2c048d2ed0f047b9ccda826b99
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-05 13:01:49 -0500

    ah

--- script.js
+++ script.js
@@ -20,14 +20,43 @@ var passwordMatches = function(event) {
 
   var current = event.target;
   var $current = $(current);
-  var password = $('#password')[0].value;
+  var password = $(event.data.searchId)[0].value;
   var confirmPassword = current.value;
 
-  if (password !== confirmPassword) {
+  if(!password.length || !confirmPassword.length){
+
+    $('#p-matching-message').text("");
+  }else if(password !== confirmPassword) {
     $('#p-matching-message').text("Your password don't match");
+  }else{
+    $('#p-matching-message').text("Your passwords match!");
+    return true
+  }
+  return false
+};
+
+var validations = function(event){
+  event.preventDefault()
+  var lengthText = validationsLength( $('input[type="text"]')[0], 32, 4)
+  var lengthTextFeild = validationsLength()
+  var lengthPassword = validationsLength()
+  var matching = passwordMatches()
+};
+
+var validationsLength = function(target, max, min){
+  var value = target.value
+  var length = value.length
+
+  if(length > max){
+    return "too long!"
+  }else if(length < min){
+    return "too short"
+  }else{
+    return true 
   }
 
 };
+
 
 var formListener = function() {
 
@@ -35,8 +64,10 @@ var formListener = function() {
   $('input[type="text"]').keyup({ number: 32 }, displayCharsRemaining );
   $('input[type="password"]').keyup({ number: 16 }, displayCharsRemaining );
 
-  $('#p-confirmation').keyup( passwordMatches );
+  $('#p-confirmation').keyup( { searchId: '#password' }, passwordMatches );
+  $('#password').keyup( { searchId: '#p-confirmation' }, passwordMatches );
 
+  $('input[type="submit"]').click(validations);
 };
 
 
