5e7c7d6d07b50686e26681b29f79fdcc346f524b
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-05 16:29:38 -0600

    mouse crosshairs

--- photo_tagging/script.js
+++ photo_tagging/script.js
@@ -1,15 +1,26 @@
 var imgFeatures = {
   
+  targetSelected: function(e) {
+
+    // plop down box
+    var x = e.pageX;
+    var y = e.pageY;
+    var img = e.target;
+
+    console.log('clicked');
+
+    $('<div>').insertAfter(img)
+      .css('background: red, height: 100px, width: 100px ');
+
+  },
 
 }
 
 var imgListener = function(){
-
-}
-
-
+  $('.first-img').on('click', imgFeatures.targetSelected);
+};
 
 
 $(document).ready(function() {
-  imgListener()
+  imgListener();
 });
