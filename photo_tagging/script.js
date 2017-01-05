797ab479c0f7d5a88461e7fcafcc04a7e742b089
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-05 17:41:02 -0500

    position

--- photo_tagging/script.js
+++ photo_tagging/script.js
@@ -2,15 +2,19 @@ var imgFeatures = {
   
   targetSelected: function(e) {
 
-    // plop down box
+
     var x = e.pageX;
     var y = e.pageY;
     var img = e.target;
 
     console.log('clicked');
 
-    $('<div>').insertAfter(img)
-      .css('background: red, height: 100px, width: 100px ');
+    $('<img src="cursor.png">').insertAfter(img)
+      .css({
+        "position": "absolute",
+        "top": y,
+        "left": x
+      })
 
   },
 
