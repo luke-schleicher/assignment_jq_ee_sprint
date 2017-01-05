c505a95fbc817357f663fb44d6be7c5bbd47957f
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-05 17:56:25 -0500

    wooooooo

--- photo_tagging/script.js
+++ photo_tagging/script.js
@@ -2,19 +2,32 @@ var imgFeatures = {
   
   targetSelected: function(e) {
 
-
     var x = e.pageX;
     var y = e.pageY;
     var img = e.target;
+    var $img = $('.first-img')
+    if $img
+      $img.css("cursor", "auto")
 
-    console.log('clicked');
+      $('<img src="cursor.png">').insertAfter(img)
+        .css({
+          "position": "absolute",
+          "top": y,
+          "left": x
+        })
 
-    $('<img src="cursor.png">').insertAfter(img)
-      .css({
-        "position": "absolute",
-        "top": y,
-        "left": x
-      })
+      $('<select>').insertAfter(img)
+        .css({
+          "position": "absolute",
+          "top": y + 120,
+          "left": x
+        }).html("<option class= 'no-option'>  </option>")
+        .append("<option> Orange </option>")
+        .append("<option> Grape </option>")
+        .append("<option> Mango </option>")
+        .append("<option> Bob </option>")
+    else 
+
 
   },
 
