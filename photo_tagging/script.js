7ba429b8f49cc9fc1fd4046d6d8169a08d95242c
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-05 17:43:29 -0600

    click off functionality

--- photo_tagging/script.js
+++ photo_tagging/script.js
@@ -5,16 +5,34 @@ var imgFeatures = {
     var x = e.pageX;
     var y = e.pageY;
     var img = e.target;
-    var $img = $('.first-img')
-    if $img
-      $img.css("cursor", "auto")
+    var $img = $('.first-img');
+
+    if ($img.hasClass('is-selecting') === true) {
+
+      $img.removeClass('is-selecting');
+
+      $img.css("cursor", "url(cursor.png), crosshair");
+
+      var $lastImage = $('img').last();
+
+      if (!$lastImage.hasClass('saved')) {
+        $lastImage.remove();
+      }
+      
+      $('select').last().remove();
+
+    } else {
+
+      $img.addClass('is-selecting');
+
+      $img.css("cursor", "auto");
 
       $('<img src="cursor.png">').insertAfter(img)
         .css({
           "position": "absolute",
           "top": y,
           "left": x
-        })
+        });
 
       $('<select>').insertAfter(img)
         .css({
@@ -25,15 +43,37 @@ var imgFeatures = {
         .append("<option> Orange </option>")
         .append("<option> Grape </option>")
         .append("<option> Mango </option>")
-        .append("<option> Bob </option>")
-    else 
-
+        .append("<option> Bob </option>");
+    }
 
   },
+
+  nameSelect: function(e) {
+
+    console.log('name select ran');
+
+    var $select = $('select').last();
+
+    if ($select.hasClass('no-option')) {
+    console.log('if ran');
+    } else {
+    console.log('else ran');
+      // keep box
+      // remove dropdown
+      // add span w/ name
+
+      $('img').last().addClass('saved');
+
+
+
+    }
+
+  }
 
 }
 
 var imgListener = function(){
+  $('body').on('change', 'select', imgFeatures.nameSelect);
   $('.first-img').on('click', imgFeatures.targetSelected);
 };
 
